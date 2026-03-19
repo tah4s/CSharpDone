993a3a96a4dcfc14891ea7cae453719fbe29b657
Taha Yusuf Sarıbaş <tahayusufsaribas@gmail.com>
2026-03-19 20:58:43 +0300

    inital commit

--- 02_Veriables/Program.cs
+++ 02_Veriables/Program.cs
@@ -1,1 +1,74 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Veriables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Double değişkenler
+
+
+            //double number;
+            //number = 4.85;
+            //Console.WriteLine(number);
+
+            Console.WriteLine("***** Fiyat Listesi *****");
+            Console.WriteLine();
+
+            double applePrice, orangePrice, strawberryPrice, patatoPrice, tomatoPrice;
+            applePrice = 14.85;
+            orangePrice = 20.95;
+            strawberryPrice = 45;
+            patatoPrice = 9.74;
+            tomatoPrice = 6.88;
+
+            Console.WriteLine("---- Elma Birim Fiyatı:" + applePrice + " TL");
+            Console.WriteLine("---- Portakal Birim Fiyatı:" + orangePrice + " TL");
+            Console.WriteLine("---- Çilek Birim Fiyatı:" + strawberryPrice + " TL");
+            Console.WriteLine("---- Patates Birim Fiyatı:" + patatoPrice + " TL");
+            Console.WriteLine("---- Domates Birim Fiyatı:" + tomatoPrice + " TL");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            double appleGram, orangeGram, strawberryGram, patatoGram, tomatoGram;
+
+            appleGram = 1.245;
+            orangeGram = 2.650;
+            strawberryGram = 0.750;
+            patatoGram = 4.859;
+            tomatoGram = 3.745;
+
+            double appleTotalPrice = applePrice * appleGram;
+            double orangeTotalPrice = orangePrice * orangeGram;
+            double strawberryTotalPrice = strawberryPrice * strawberryGram;
+            double patatoTotalPrice = patatoPrice * patatoGram;
+            double tomatoTotalPrice = tomatoPrice * tomatoGram;
+
+            Console.WriteLine("Alınan Ürün: Elma - " + "Birim Fiyat: " + applePrice + " - Gramaj: " + appleGram + " - Toplam Tutar: " + appleTotalPrice);
+            Console.WriteLine("Alınan Ürün: Portakal - " + "Birim Fiyat: " + orangePrice + " - Gramaj: " + orangeGram + " - Toplam Tutar: " + orangeTotalPrice);
+            Console.WriteLine("Alınan Ürün: Çilek - " + "Birim Fiyat: " + strawberryPrice + " - Gramaj: " + strawberryGram + " - Toplam Tutar: " + strawberryTotalPrice);
+            Console.WriteLine("Alınan Ürün: Patates - " + "Birim Fiyat: " + patatoPrice + " - Gramaj: " + patatoGram + " - Toplam Tutar: " + patatoTotalPrice);
+            Console.WriteLine("Alınan Ürün: Domates - " + "Birim Fiyat: " + tomatoPrice + " - Gramaj: " + tomatoGram + " - Toplam Tutar: " + tomatoTotalPrice);
+
+            double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + patatoTotalPrice + tomatoTotalPrice;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Alışveriş Toplam Tutar: " + shoppingTotalPrice + " TL");
+
+            #endregion
+
+            Console.Read();
+            
+        }
+    }
+}
